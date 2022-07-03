02b3e27d2b8804f7cf04f734a6f87bafb93c2f27
c-wrld <108183605+c-wrld@users.noreply.github.com>
2022-07-03 18:14:22 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+//show POP cont
+function show(){
+ document.getElementById('menu-btn').style.display = "none";
+ document.getElementById('pop').style.display = "block";
+ document.getElementById('cancel-btn').style.display = "block";
+
+//console
+console.log('OK');//SYS==OK
+}
+//hide POP cont
+function hide(){
+ document.getElementById('menu-btn').style.display = "block";
+ document.getElementById('pop').style.display = "none";
+ document.getElementById('cancel-btn').style.display = "none";
+
+//console
+ console.log('DONE');//SYS==done
+}
